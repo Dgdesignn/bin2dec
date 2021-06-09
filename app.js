e3c3ec5f7b0bfc5c692fd2d7b867cb079c02e3a3
Dgdesign <dg2danielgeral@gmail.com>
2021-06-09 15:29:42 +0200

    Bin2Dec

---
+++ app.js
@@ -0,0 +1,77 @@
+const btnCalc = document.querySelector('.btn-calc');
+const inputGetter = document.querySelector('.input-numbers');
+const display_result = document.querySelector('.result');
+const error_mensage = document.querySelector('.error-mensage');
+const form = document.querySelector('#form-calc');
+
+
+
+btnCalc.addEventListener('click',()=>{
+
+    let if_is_binary = isBinary(inputGetter.value),
+        conversion = binToDec(inputGetter.value);
+
+        if(if_is_binary){
+            display_result.textContent = conversion
+        }else{
+            error_mensage.textContent = 'O número introduzido não é binário'
+            display_result.textContent = 0
+        }
+    
+    
+    
+    inputGetter.value = '';
+})
+
+
+
+
+
+//function conversor
+function binToDec(value){
+
+    if(isBinary(value)){
+               //varíavel que vai armazenar o resultado
+               let sum = 0;
+               //Transforming into array
+               let binary_array = value.split('');
+               //reverse binary_array array
+               binary_array = binary_array.reverse();
+               //get array size
+               let size = (binary_array.length - 1);
+       
+               for(let counter = size; counter >= 0; counter--){
+                   sum += (binary_array[counter]*(2**counter));//this is the expression conversor
+               }
+               return sum;
+    }else{
+        return 'there is not valid binary';
+    }
+
+  
+}
+
+//function to verify if binary
+function isBinary(value){
+
+    let
+        is_bin = 0,
+        numbers = value.split(''),
+        array_size = value.length - 1,
+        decimal_numbers = [2,3,4,5,6,7,8,9];
+
+    //verify if the input values is binary
+    for(let i = 0; i <= array_size; i++){
+        let response = (decimal_numbers.indexOf(Number(numbers[i])))==-1?true:false;
+        if(response){
+            is_bin++;
+        }
+    }
+
+    if(is_bin == (array_size+1)){
+        return true;
+    }else{
+        return false;
+    }
+
+}
